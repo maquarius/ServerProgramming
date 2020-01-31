7ce14281f20012a29c12be6d6359587efc447948
Marius van der Busse <mariusvanderbusse@hotmail.com>
2020-01-31 11:21:37 +0200

    check if this change uploads

--- excercisesWeek1/ControllerAndEndpoint/src/main/java/com/example/demo/web/HelloControler.java
+++ excercisesWeek1/ControllerAndEndpoint/src/main/java/com/example/demo/web/HelloControler.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloControler {
 
 	@RequestMapping("/hello")
-	public String hello(@RequestParam(name="name", required=false, defaultValue="Marius")String name,@RequestParam(value="location", required=false, defaultValue="Moon") String location) {
-		return "Welcome to the " + location + " " + name + " !";
+	public String hello(@RequestParam(name="name", required=false, defaultValue="Marius")String name,@RequestParam(value="location", required=false, defaultValue="Github") String location) {
+		return "Welcome to the " + location + ", " + name + " !";
 	}
 }
