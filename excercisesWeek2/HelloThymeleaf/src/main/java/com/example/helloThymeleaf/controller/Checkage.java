f60082664d45fe356fda97e873d7cd28a73358a5
Marius van der Busse <mariusvanderbusse@hotmail.com>
2020-02-01 12:16:05 +0200

    "Added GET reqeust mapping"

--- excercisesWeek2/HelloThymeleaf/src/main/java/com/example/helloThymeleaf/controller/Checkage.java
+++ excercisesWeek2/HelloThymeleaf/src/main/java/com/example/helloThymeleaf/controller/Checkage.java
@@ -3,13 +3,15 @@ package com.example.helloThymeleaf.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Checkage {
-	
-	@RequestMapping("/Checkage")
-	public String ageCheck(@RequestParam(value="age")int age, @RequestParam(value="name") String name, Model model) {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String ageCheck(@RequestParam(value = "age") int age, @RequestParam(value = "name") String name,
+			Model model) {
 		model.addAttribute("name", name);
 		model.addAttribute("age", age);
 		return "Checkage";
