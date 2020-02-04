52a65016ed5184eb7c8da2506cb706aa85c48e48
maquarius <mariusvanderbusse@hotmail.com>
2020-02-04 18:18:28 +0200

    Added beginning excercise 3 and added databse to bookstore

--- excercisesWeek2/Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
+++ excercisesWeek2/Bookstore/src/main/java/com/example/Bookstore/web/BookController.java
@@ -1,18 +1,22 @@
 package com.example.Bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.Bookstore.domain.Book;
+import com.example.Bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+	@Autowired
+	private BookRepository repository;
 
-	@RequestMapping(value="/index", method = RequestMethod.GET)
+	@RequestMapping(value={"/index", "/"}, method = RequestMethod.GET)
 	public String getBooks(Model model) {
-		model.addAttribute("book", new Book("Harry Potter","J.K. Rowling", 2003,1391238123, 23.50));
-		return "index";
+		model.addAttribute("book", repository.findAll());
+				return "index";
 	}
 }	
